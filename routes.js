c25d87cc3e8a706312920c767dc842726ebcad36
Zachary Hughes <zhughes@captechventures.com>
2018-04-27 16:19:53 -0400

    all routes are working; interfacing with dynamodb properly

--- routes.js
+++ routes.js
@@ -1,10 +1,38 @@
 'use strict';
 
-const routes = [{
+let dbController = require('./controllers/DBController');
+
+const routes = [
+{
 	method: 'GET',
 	path: '/ping',
 	handler: (request, h) => {
 		return 'pong';
+	}
+},
+{
+	method: 'GET',
+	path: '/blog',
+	handler: (request, h) => {
+		return dbController.readAll()
+			.then(data => {
+				return data;
+			}).catch(err => {
+				return err;
+			});
+	}
+},
+{
+	method: 'GET',
+	path: '/blog/{id}',
+	handler: (request, h) => {
+		let id = encodeURIComponent(request.params.id);
+		return dbController.read(id)
+			.then(data => {
+				return data;
+			}).catch(err => {
+				return err;
+			});
 	}
 }];
 
