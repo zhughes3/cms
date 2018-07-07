290260354875e33ac432531552d74479cd1fca2a
Zachary Hughes <zhughes@manh.com>
2018-07-07 14:27:43 -0400

    added new routes, added create option for blurbs, integrated into routes

--- services/BlurbController.js
+++ services/BlurbController.js
@@ -4,6 +4,7 @@ let AWS = require('aws-sdk');
 AWS.config.update({region: 'us-east-1'});
 AWS.config.credentials = new AWS.SharedIniFileCredentials({profile: 'captech'});
 let docClient = new AWS.DynamoDB.DocumentClient();
+const uuidv4 = require('uuid/v4');
 
 const TABLE_NAME = 'zhughes-blurbs';
 
@@ -21,6 +22,36 @@ module.exports = {
 				}
 			});
 		});
-    }
+	},
+	create: (payload) => {
+		let blurb = _formatOnCreateItem(payload);
+		let params = {
+			TableName: TABLE_NAME,
+			Item: blurb
+		};
+		return new Promise((resolve, reject) => {
+			docClient.put(params, (err, data) => {
+				if (err) {
+					reject(err);
+				} else {
+					resolve(item);
+				}
+			});
+		});
+	}
 
 };
+
+function _formatOnCreateItem(payload) {
+	const type = payload.type;
+	const date = new Date().toISOString();
+	const id = uuidv4();
+	const body = payload.body;
+	return {
+		_id: id,
+		date: date,
+		type: type,
+		body: body
+
+	};
+}
--- services/BlurbRoutes.js
+++ services/BlurbRoutes.js
@@ -15,5 +15,19 @@ const routes = [
                     return err;
                 });
         }
+    },
+    {
+        method: 'POST',
+        path: '/blurbs',
+        handler: (request, h) => {
+            return blurbController.create(request.payload)
+                .then(data => {
+                    return "Blurb created successfully."
+                }).catch(err => {
+                    return err;
+                });
+        }
     }
 ];
+
+module.exports = routes;
